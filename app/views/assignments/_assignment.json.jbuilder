51c1400f81d6adffd0301f28f4ab4a06d4b12217
Miłosz Sędziak <m.sedziak01@gmail.com>
2017-02-25 03:30:39 +0100

    Create Assignments CRUD and prepear form to create association

---
+++ app/views/assignments/_assignment.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! assignment, :id, :zombie_id, :role_id, :created_at, :updated_at
+json.url assignment_url(assignment, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :roles
+  resources :assignments
   get 'rotting_zombies', to: 'rotting_zombies#index'
 
   resources :zombies do
