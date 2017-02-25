2e8bd41977989e516f5e717c9a3d084d13b40665
Miłosz Sędziak <m.sedziak01@gmail.com>
2017-02-25 03:03:02 +0100

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+puts 'Seeds: start'
+ROLES = ['Captain', 'Brain tester', 'Scout', 'Medic', 'Tanker', 'Assassin'].freeze
+ROLES.each { |role| Role.create!(title: role) }
+DECOMPS = ['Fresh', 'Rotting', 'Stale', 'Dead (again)']
+20.times do
+  name = Faker::Name.name
+  Zombie.create!(
+  name: name,
+  bio: Faker::Cat.name,
+  age: Faker::Number.between(1, 60),
+  email: Faker::Internet.free_email(name.split.join('.')),
+  decomp: DECOMPS[rand(4)]
+  )
+  Zombie.last.create_brain(
+  flavor: Faker::Food.ingredient,
+  status: Faker::Hipster.word
+  )
+end
+
+zombies = Zombie.all
+Role.all.each do |role|
+  role.zombies << zombies.sample(rand(1..3))
+end
+
+zombies.each do |zombie|
+  rand(15).times do
+    zombie.tweets.create!(
+    status: Faker::Twitter.status[:text]
+    )
+  end
+end
+
+puts 'Seeds: finish'
